0988a0721b71fa518e3d12cb67f312a44173897c
MuhammadSaputraArjunaidy <mhmmdsptraa15@gmail.com>
2025-11-15 17:27:08 +0800

    menambah halaman profil

--- template/header.php
+++ template/header.php
@@ -25,7 +25,7 @@
 						<li><a href="#">About</a></li>
 						<li><a href="#">Courses</a></li>
 						<li><a href="blog.html">Blog</a></li>
-						<li><a href="contact.html">Contact</a></li>
+						<li><a href="profil.php">Telpon</a></li>
 					</ul>
 				</nav>
 				<!-- /Navigation -->
