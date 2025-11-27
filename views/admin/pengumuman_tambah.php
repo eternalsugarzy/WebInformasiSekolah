a0e79592897006a3c1879baf509d4d0cd5a98521
AlmasSyauqannanda <almassyauqannanda@gmail.com>
2025-11-27 21:46:19 +0800

    update

--- views/admin/pengumuman_tambah.php
+++ views/admin/pengumuman_tambah.php
@@ -1,3 +1,8 @@
+<?php 
+$title = "Tambah Galeri";
+require_once 'template/header.php'; 
+require_once 'template/sidebar.php'; 
+?>
 <!DOCTYPE html>
 <html lang="id">
 <head>
@@ -8,44 +13,14 @@
     <link type="text/css" rel="stylesheet" href="../css/bootstrap.min.css"/>
     <link rel="stylesheet" href="../css/font-awesome.min.css">
     <link type="text/css" rel="stylesheet" href="../css/style.css"/>
-    <style>
-        body { background-color: #f5f7fa; font-family: 'Montserrat', sans-serif; padding-top: 0; }
-        .admin-sidebar { width: 260px; height: 100vh; background: #2B2D42; position: fixed; left: 0; top: 0; z-index: 100; }
-        .admin-brand { height: 70px; display: flex; align-items: center; padding-left: 30px; background: #252739; border-bottom: 1px solid #3a3c55; }
-        .admin-brand h3 { color: #fff; margin: 0; font-size: 18px; font-weight: 700; text-transform: uppercase; }
-        .sidebar-menu { list-style: none; padding: 20px 0; margin: 0; }
-        .sidebar-menu li a { display: block; padding: 15px 30px; color: #b7c0cd; text-decoration: none; transition: 0.3s; border-left: 4px solid transparent; }
-        .sidebar-menu li a:hover, .sidebar-menu li.active a { background: #32344d; color: #fff; border-left-color: #FF6700; }
-        .sidebar-menu li a i { width: 25px; margin-right: 10px; }
-        .main-content { margin-left: 260px; }
-        .admin-header { background: #fff; height: 70px; box-shadow: 0 2px 5px rgba(0,0,0,0.05); display: flex; justify-content: space-between; align-items: center; padding: 0 40px; }
-        .content-wrapper { padding: 40px; }
-        .card-box { background: #fff; padding: 30px; border-radius: 8px; box-shadow: 0 5px 15px rgba(0,0,0,0.05); }
-    </style>
+    
 </head>
 <body>
 
-    <div class="admin-sidebar">
-        <div class="admin-brand"><h3>Edu<span>Site</span> Admin</h3></div>
-        <ul class="sidebar-menu">
-            <li><a href="index.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
-            <li><a href="berita.php"><i class="fa fa-newspaper-o"></i> Kelola Berita</a></li>
-            <li class="active"><a href="pengumuman.php"><i class="fa fa-bullhorn"></i> Kelola Pengumuman</a></li>
-            <li><a href="#"><i class="fa fa-users"></i> Data Guru</a></li>
-            <li><a href="#"><i class="fa fa-graduation-cap"></i> Info PPDB</a></li>
-            <li style="margin-top: 30px; border-top: 1px solid #3a3c55;">
-                <a href="../index.php" target="_blank"><i class="fa fa-external-link"></i> Lihat Website</a>
-            </li>
-        </ul>
-    </div>
+    
 
     <div class="main-content">
-        <div class="admin-header">
-            <h4 style="margin:0; font-weight:700; color: #333;">TAMBAH PENGUMUMAN</h4>
-            <div>
-                <span style="margin-right: 15px;">Halo, <b><?php echo $_SESSION['admin_nama']; ?></b></span>
-            </div>
-        </div>
+    <?php require_once 'template/topbar.php'; ?>
 
         <div class="content-wrapper">
             <div class="card-box">
@@ -89,3 +64,5 @@
 
 </body>
 </html>
+
+<?php require_once 'template/footer.php'; ?>
